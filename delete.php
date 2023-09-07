33e3479419329ba865b78a99fe7757b7faaded8a
MalingaMp <malingace12@gmail.com>
2023-09-08 02:43:25 +0530

    changes in alert message

--- delete.php
+++ delete.php
@@ -8,7 +8,11 @@ $query = "DELETE FROM users WHERE id='$id' ";
 $data = mysqli_query($conn, $query);
 
 if ($data) {
-    echo "<script>alert('Record Deleted')</script>";
+    echo "<script>
+    setTimeout(function() {
+        alert('Record Deleted');
+    }, 0); // Change the delay (in milliseconds) to control how long the alert is displayed
+</script>";
 ?>
     <meta http-equiv="refresh" content="0; url = http://localhost/home/display.php" />
 <?php
